22b4f97789187a72ec5f60abd6b99df09decdc51
Ramza13 <52087122+Ramza13@users.noreply.github.com>
2020-08-09 21:39:40 -0400

    Fix clang tidy

--- src/generic_trigger_op_on_precon.h
+++ src/generic_trigger_op_on_precon.h
@@ -14,6 +14,6 @@ namespace generic_trigger_op_on_precon
 void load_generic_trigger_op_on_precon( const JsonObject &jo, const std::string & );
 void queue_generic_operation( time_duration duration, generic_operation_type_id id );
 void process_operations();
-}
+} // namespace generic_trigger_op_on_precon
 
 #endif // CATA_SRC_GENERIC_TRIGGER_OP_ON_PRECON_H
