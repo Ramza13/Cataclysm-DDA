974433e54c2cf65630d22268f600f6334a20cd51
Ramza13 <52087122+Ramza13@users.noreply.github.com>
2020-08-15 09:02:39 -0400

    Build fix attempt

--- src/generic_precondition_type.cpp
+++ src/generic_precondition_type.cpp
@@ -108,7 +108,7 @@ void generic_precondition_type::load( const JsonObject &jo, const std::string & 
     }
     if( jo.has_member( "time_of_day" ) ) {
         time_of_day time_load = time_of_day::both;
-        mandatory( jo, was_loaded, "time_of_day", time_load );
+        optional( jo, was_loaded, "time_of_day", time_load, time_of_day::both );
         preconditions.push_back( new time_of_day_precondition( time_load ) );
     }
     if( jo.has_member( "height_min" ) || jo.has_member( "height_max" ) ) {
